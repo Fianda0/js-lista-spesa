b37ef4b7c1f25af0df1d9d33946879c01fd6b6ad
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-04-24 15:27:26 +0200

    M2

--- main.js
+++ main.js
@@ -10,8 +10,14 @@ const listaSpesa = [
 
 const listElement = document.querySelector('ul');
 
-for (let i = 0; i < listaSpesa.length; i++) {
+// for (let i = 0; i < listaSpesa.length; i++) {
+//     const element = listaSpesa[i];
+//     console.log(element)
+// }
+
+let i = 0;
+while (i < listaSpesa.length) {
     const element = listaSpesa[i];
     console.log(element)
+    i++
 }
-
