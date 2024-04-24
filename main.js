e953460f0db58d34c91e321a597c75d23abe0f13
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-04-24 15:26:54 +0200

    M1

---
+++ main.js
@@ -0,0 +1,17 @@
+const listaSpesa = [
+    'pane',
+    'pizza',
+    'pasta',
+    'latte',
+    'biscotti',
+    'acqua',
+    'sale'
+];
+
+const listElement = document.querySelector('ul');
+
+for (let i = 0; i < listaSpesa.length; i++) {
+    const element = listaSpesa[i];
+    console.log(element)
+}
+
